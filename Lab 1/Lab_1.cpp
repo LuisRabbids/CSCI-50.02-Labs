6a4112d00902a5d67f56fa71fcb2e4dcc3a06971
Luis Felipe <luisfelipecodes@gmail.com>
2026-01-14 09:21:33 +0800

    Initial commit

---
+++ Lab 1/Lab_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void){
+    string name;                         // instantiate a string variable
+    cout << "What is your name?: ";      // prompt and ask user for their name         
+    cin >> name;                         // read user's input
+    cout << name << endl;                        // print out the name
+
+    // 3. Print out the user’s name with only the first letter revealed and all other letters replaced with #
+
+    // 4. Print out the user’s name with all the letters replaced with the next letter
+
+    // 5. Function that uses a character pointer argument 
+
+    return 0;
+}
