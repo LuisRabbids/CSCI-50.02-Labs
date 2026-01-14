349f21293cf49c1da50717f072b0aaf5716734c0
Luis Felipe <luisfelipecodes@gmail.com>
2026-01-14 09:44:48 +0800

    Added part 3, Print out the user’s name with only the first letter revealed and all other letters replaced with #

--- Lab 1/Lab1_FelipeOlpindoScarlata.cpp
+++ Lab 1/Lab1_FelipeOlpindoScarlata.cpp
@@ -3,14 +3,21 @@ using namespace std;
 
 
 int main(void){
-    string name;                         // instantiate a string variable
-    cout << "What is your name?: ";      // prompt and ask user for their name         
-    cin >> name;                         // read user's input
-    cout << name << endl;                // print out the name
+    string name;                                          // instantiate a string variable
+    cout << "What is your name?: ";                       // prompt and ask user for their name         
+    cin >> name;                                          // read user's input
+    cout << name << endl;                                 // print out the name
 
-    // 3. Print out the user’s name with only the first letter revealed and all other letters replaced with #
+
+    cout << name[0];                                      // print out the first letter of the name               
+    for (int i = 0; i < name.length() - 1; i++) {         // loop through the rest of the letters
+        cout << "#";                                      // print out "#" for each remaining letter
+    }
+    cout << endl;                                         // print a newline
+
 
     // 4. Print out the user’s name with all the letters replaced with the next letter
+
 
     // 5. Function that uses a character pointer argument 
 
