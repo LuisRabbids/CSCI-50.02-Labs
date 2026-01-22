b283a2824a7deeaea08181c92bcb952b638aaa5a
Juan Olpindo <dad3ztroyah@gmail.com>
2026-01-22 22:37:16 +0800

    Fixed comments

--- Lab 2/Lab2_FelipeOlpindoScarlata.cpp
+++ Lab 2/Lab2_FelipeOlpindoScarlata.cpp
@@ -2,18 +2,18 @@
 #include <string>
 using namespace std;
 
-bool isNum(string line)
+bool isNum(string line)             // boolean function to check if the given string is an integer
 {
     char* p;
-    strtol(line.c_str(), &p, 10);
-    return *p == 0;
+    strtol(line.c_str(), &p, 10);   // converts the string into an integer of base 10 and puts into p the value of the rest of the string after the first integer
+    return *p == 0;                 // true if the string is only an integer, false if it is multiple integers or has a non-integer data type in it
 }
 
 int main(void){
     int numAgents;
     cin >> numAgents;
 
-    for (int i = 1; i-1 < numAgents; i++){
+    for (int i = 1; i-1 < numAgents; i++){          // start at 1 because the Agent count starts at 1
         int x, y;
         cin >> x >> y;              // read in the coordinates of the current agent
         cin.ignore(1, ' ');         // clean up the single space left after 'y'
@@ -23,9 +23,7 @@ int main(void){
 
         cout << "Agent #" << i << " is at (" << x << ", " << y << ")" << endl;      // print the coordinate of current agent
 
-        //UPDATE TO TAKE INTO ACCOUNT LARGER NUMBERS (multiple digits)
-        //update to check if message is a number
-        if ( isNum(message)) {                                               // check if the message length is 1 (which is not always so UPDATE)
+        if ( isNum(message)) {                                                      // check if the message is only an integer
             cout << "Agent #" << i << " holds up the number: " << message << endl;  // print number held up by agent
         } else {
             cout << "Agent #" << i << " yells: \"" << message << "\"" << endl;      // print message yelled by agent
