49f99ec467b507c7d7d489f847b3928aef19ef1d
Luis Felipe <luisfelipecodes@gmail.com>
2026-01-22 10:32:08 +0800

    First version of Lab 2

---
+++ Lab 2/Lab2_FelipeOlpindoScarlata.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void){
+    int numAgents;
+    cin >> numAgents;
+
+    for (int i = 0; i < numAgents; i++){
+        int x, y;
+        cin >> x >> y;              // read in the coordinates of the current agent
+        cin.ignore(1, ' ');         // clean up the single space left after 'y'
+
+        string message;
+        getline(cin, message);      // get the rest of the line as the message
+
+        cout << "Agent #" << i << " is at (" << x << ", " << y << ")" << endl;      // print the coordinate of current agent
+
+        //UPDATE TO TAKE INTO ACCOUNT LARGER NUMBERS (multiple digits)
+        if ( message.length() == 1) {                                               // check if the message length is 1 (which is not always so UPDATE)
+            cout << "Agent #" << i << " holds up the number: " << message << endl;  // print number held up by agent
+        } else {
+            cout << "Agent #" << i << " yells: \"" << message << "\"" << endl;      // print message yelled by agent
+        }
+    }
+
+    return 0;
+}
