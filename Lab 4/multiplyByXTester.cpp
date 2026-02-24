3dc924bbbf841f0d725337e7cd5289625122cef8
Luis Felipe <luisfelipecodes@gmail.com>
2026-02-24 23:03:10 +0800

    Added multiplyByXTester cpp file

---
+++ Lab 4/multiplyByXTester.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "IntArray.h"
+
+
+extern void multiplyByX(IntArray *p, int x);
+
+
+int main() {
+    // - create a new IntArray with your own elements
+    // (don't forget to properly allocate/deallocate the memory!)
+    // - print it out
+    // - multiply it by a number of your choosing via multiplyByX()
+    // - print the result
+    return 0;
+}
