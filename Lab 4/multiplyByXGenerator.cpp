706c1522478eadb9f3d49006e3e8dc908658cb6f
Juan Olpindo <dad3ztroyah@gmail.com>
2026-02-26 15:14:09 +0800

    Fixed mangling logic

--- Lab 4/multiplyByXGenerator.cpp
+++ Lab 4/multiplyByXGenerator.cpp
@@ -4,20 +4,24 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    string input = argv[1];
     int x = atoi(argv[1]);
 
     cout << "\t.file\t\"multiplyBy" << x <<".cpp\"" << endl;
     cout << "\t.text" << endl;
-    cout << "\t.globl\t_Z11multiplyBy" << x << "P8IntArrayi" << endl;
-    cout << "\t.type\t_Z11multiplyBy" << x << "P8IntArrayi, @function" << endl;
-    cout << "_Z11multiplyByXP8IntArrayi:" << endl;
+    cout << "\t.globl\t_Z" << 10 + input.length() << "multiplyBy" << x << "P8IntArrayi" << endl;
+    cout << "\t.type\t_Z" << 10 + input.length() << "multiplyBy" << x << "P8IntArrayi, @function" << endl;
+    cout << "_Z" << 10 + input.length() << "multiplyByXP8IntArrayi:" << endl;
 
     cout << ".LFB0:" << endl;
     cout << "\t.cfi_startproc" << endl;
     cout << "\tendbr64" << endl;
+
+    cout << "\tpushq\t%rbp" << endl;
     
     return 0;
 }
 
 // https://learn.microsoft.com/en-us/cpp/cpp/main-function-command-line-args?view=msvc-170
 // https://www.geeksforgeeks.org/cpp/cpp-program-for-char-to-int-conversion/
+// https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangling
