16635d259f604be858da6989d996fad751d59484
Luis Felipe <luisfelipecodes@gmail.com>
2026-02-26 14:22:00 +0800

    Added comments to multiplyByX cpp

--- Lab 4/multiplyByX.cpp
+++ Lab 4/multiplyByX.cpp
@@ -2,9 +2,9 @@
 
 void multiplyByX(IntArray *p, int x)
 {
-    int n = p->size;
-    int *arr = p->elements;
+    int n = p->size;                 // Get the size of the array from the struct
+    int *arr = p->elements;          // Get the pointer to the actual integer array from the struct
     for (int i = 0; i < n; ++i) {
-        arr[i] = arr[i] * x;
+        arr[i] = arr[i] * x;         // Multiply current element by x and assign it back to overwrite the old value
     }
 }
